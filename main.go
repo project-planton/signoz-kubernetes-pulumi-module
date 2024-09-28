61aefa8e4e6f0881e5816228ce5eef288bf1470c
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-28 13:22:36 +0530

    renamed planton to project-planton

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/planton-cloud-kubernetes-pulumi-module/pkg"
-	"github.com/plantoncloud/planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/signozkubernetes"
+	"github.com/plantoncloud/project-planton-cloud-kubernetes-pulumi-module/pkg"
+	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/signozkubernetes"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
