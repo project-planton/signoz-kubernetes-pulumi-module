fc383aecb45dd6bc6bf50d20cb1057284aea4815
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:58:04 +0530

    switched over to using local generated stubs for project-planton apis

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	signozkubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/signozkubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
+	signozkubernetesv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/kubernetes/signozkubernetes/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/stackinput"
 	"github.com/project-planton/signoz-kubernetes-pulumi-module/pkg"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
--- pkg/main.go
+++ pkg/main.go
@@ -1,9 +1,9 @@
 package pkg
 
 import (
-	signozkubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/signozkubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
+	signozkubernetesv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/kubernetes/signozkubernetes/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/provider/kubernetes/pulumikubernetesprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
